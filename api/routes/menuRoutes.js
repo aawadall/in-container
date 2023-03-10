336064048820ed9f23d5b73398c8ddc25438ad2a
Ahmed Awadallah <aawadall@ualberta.ca>
2023-03-10 08:53:59 -0500

    correct controller path

--- api/routes/menuRoutes.js
+++ api/routes/menuRoutes.js
@@ -6,7 +6,7 @@ const express = require('express');
 const router = express.Router();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
-const menu_controller = require('../controllers/menuController');
+const menu_controller = require('../controllers/menuControllers');
 
 // endpoints 
 router.get('/', menu_controller.menu_list);
