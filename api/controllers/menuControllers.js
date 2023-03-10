e7addf6127d0500f06f6d6f2510eb14a6b60500b
Ahmed Awadallah <aawadall@ualberta.ca>
2023-03-10 08:56:34 -0500

    issue at controller

--- api/controllers/menuControllers.js
+++ api/controllers/menuControllers.js
@@ -8,6 +8,9 @@ const Menu = require('../models/menuModel');
 exports.menu_list = function (req, res) {
     Menu.find({}, function (err, menu) {
         if (err) return next(err);
+
+        // peek into menu 
+        console.log(menu);
         res.send(menu);
     })
 }
