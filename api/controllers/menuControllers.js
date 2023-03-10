40577e1454959ac44da1d70b42720fd8c7793ece
Ahmed Awadallah <aawadall@ualberta.ca>
2023-03-10 09:02:25 -0500

    workaround for menu service

--- api/controllers/menuControllers.js
+++ api/controllers/menuControllers.js
@@ -6,11 +6,12 @@ const Menu = require('../models/menuModel');
 
 // List all menu items
 exports.menu_list = function (req, res) {
-    Menu.find({}, function (err, menu) {
-        if (err) return next(err);
+    // Menu.find({}, function (err, menu) {
+    //     if (err) return next(err);
 
-        // peek into menu 
-        console.log(menu);
-        res.send(menu);
-    })
+    //     // peek into menu 
+    //     console.log(menu);
+    //     res.send(menu);
+    // })
+    res.send(Menu);
 }
